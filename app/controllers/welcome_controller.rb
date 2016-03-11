20c21c25cef71e2a61711c23e9692f9b45867655
development@propertysearchgroup.co.uk <development@propertysearchgroup.co.uk>
2016-03-11 16:21:40 +0000

    implemented ajax return on postcode button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,12 @@ class WelcomeController < ApplicationController
     def index
 
     end
+
+    def find_local_office
+        @office =
+        respond_to do |format|
+            format.js   {}
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+
     root 'welcome#index'
+    post 'welcome/find_local_office' => 'welcome#find_local_office'
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
   # root 'welcome#index'
 
   # Example of regular route:
