7bf3ec1de58103e6562b980000cda7b80314e30d
development@propertysearchgroup.co.uk <development@propertysearchgroup.co.uk>
2016-03-14 17:01:17 +0000

    search based on first part of postcode

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
         if params["postcode"].empty?
             @office=nil;
         else
-            @office = Office.where("lower(post_code) LIKE ?", "%#{params["postcode"].downcase}%");
+            @office = Office.where("lower(post_code) LIKE ?", "%#{params["postcode"].downcase.split[0]}%");
         end
 
         respond_to do |format|
