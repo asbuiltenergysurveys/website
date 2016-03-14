276eaa47f4c9f4b0945056fef5af8d735fde5747
development@propertysearchgroup.co.uk <development@propertysearchgroup.co.uk>
2016-03-14 10:46:48 +0000

    update find office view to use table fields

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,12 @@ class WelcomeController < ApplicationController
     end
 
     def find_local_office
-        @office =
+        if params["postcode"].empty?
+            @office=nil;
+        else
+            @office = Office.where("lower(name) LIKE ?", "%#{params["postcode"].downcase}%");
+        end
+
         respond_to do |format|
             format.js   {}
         end
