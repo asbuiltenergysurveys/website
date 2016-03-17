590fabcc4784d7957974734456174a8684a90a56
stevehunt <stevehunt@propertysearchgroup.co.uk>
2016-03-17 16:49:42 +0000

    resepc page added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     root 'welcome#index'
     post 'welcome/find_local_office' => 'welcome#find_local_office'
     get 'about' => 'menu#about'
+    get 'residential-epc' => 'menu#resepc'
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
