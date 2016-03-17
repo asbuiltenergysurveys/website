422e7ae98c218333dd28b951d2d0217cf047703c
stevehunt <stevehunt@propertysearchgroup.co.uk>
2016-03-17 15:59:54 +0000

    add about us page

---
+++ app/controllers/menu_controller.rb
@@ -0,0 +1,6 @@
+class MenuController < ApplicationController
+    def about
+
+    end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
     root 'welcome#index'
     post 'welcome/find_local_office' => 'welcome#find_local_office'
+    get 'about' => 'menu#about'
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
