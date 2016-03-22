4970bdf3aaea001e39f19e651bb2bf31d6f7d254
development@propertysearchgroup.co.uk <development@propertysearchgroup.co.uk>
2016-03-22 11:50:17 +0000

    tidy menu controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :news
+    resources :news
     devise_for :users, :skip => [:sessions]
     as :user do
         get 'admin' => 'devise/sessions#new', :as => :new_user_session
