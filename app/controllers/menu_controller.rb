4340de37a90e4ecd80fd4604d2e3b28d9e35d594
lmcraciun90 <lauracraciun@propertysearchgroup.co.uk>
2016-03-23 16:28:54 +0000

    fixed data retrieval issue

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,7 +1,7 @@
 class MenuController < ApplicationController
     
     def offices
-        @offices = Office.order(:name).uniq.pluck(:name, :email, :tel)
+        @offices = Office.select(:name, :email, :tel).distinct.order(:name)
     end
 
 end
